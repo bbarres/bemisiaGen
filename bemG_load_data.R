b1bdbf009802f57f94c876d0c2c54ec81bc941b4
bbarres <benoit.barres@gmail.com>
2020-05-22 17:44:31 +0200

    structure Bemisia Q from 100 to 50 runs

--- bemG_load_data.R
+++ bemG_load_data.R
@@ -18,7 +18,7 @@ source("bemG_strplot_fun.R")
 
 
 ##############################################################################/
-#Population genetic data loading####
+#Loading the population genetic data####
 ##############################################################################/
 
 bemipop<-read.table("data/bem_genepop.txt",header=TRUE,sep="\t")
@@ -37,54 +37,54 @@ bemipop<-read.table("data/bem_genepop.txt",header=TRUE,sep="\t")
 #importation: we turn spaces into tab, to separate the columns (easily done 
 #in excel for example)
 
-#for K=2, import the 100 run datafile (100 q matrix)
-K2_100runs<-read.table("data/Q_bemigen_K2.indfile",header=FALSE,
+#for K=2, import the 50 run datafile (50 q matrix)
+K2_50runs<-read.table("data/Q_bemigen_K2.indfile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
 #import the column order for the best CLUMPP "permutation"
 K2_colorder<-read.table("data/Q_bemigen_K2.colorder",header=FALSE,
                         blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 54)
-K2_100runs<-split(K2_100runs[,-c(1:5)],rep(1:100,each=54))
+K2_50runs<-split(K2_50runs[,-c(1:5)],rep(1:50,each=54))
 #reordering the columns so that the different repetition colorization fit
-for (i in 1:100){
-  K2_100runs[[i]]<-K2_100runs[[i]][as.numeric(K2_colorder[i,])]
+for (i in 1:50){
+  K2_50runs[[i]]<-K2_50runs[[i]][as.numeric(K2_colorder[i,])]
 }
 #importing the order of the run so that the different repetition 
 #corresponding to the same clustering solution followed each other
 K2_reporderQ<-read.table("data/Q_bemigen_K2.runorder",header=FALSE,
                          blank.lines.skip=TRUE,sep="\t")+1
 
-#for K=3, import the 100 run datafile (100 q matrix)
-K3_100runs<-read.table("data/Q_bemigen_K3.indfile",header=FALSE,
+#for K=3, import the 50 run datafile (50 q matrix)
+K3_50runs<-read.table("data/Q_bemigen_K3.indfile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
 #import the column order for the best CLUMPP "permutation"
 K3_colorder<-read.table("data/Q_bemigen_K3.colorder",header=FALSE,
                         blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 54)
-K3_100runs<-split(K3_100runs[,-c(1:5)],rep(1:100,each=54))
+K3_50runs<-split(K3_50runs[,-c(1:5)],rep(1:50,each=54))
 #reordering the columns so that the different repetition colorization fit
-for (i in 1:100){
-  K3_100runs[[i]]<-K3_100runs[[i]][as.numeric(K3_colorder[i,])]
+for (i in 1:50){
+  K3_50runs[[i]]<-K3_50runs[[i]][as.numeric(K3_colorder[i,])]
 }
 #importing the order of the run so that the different repetition 
 #corresponding to the same clustering solution followed each other
 K3_reporderQ<-read.table("data/Q_bemigen_K3.runorder",header=FALSE,
                          blank.lines.skip=TRUE,sep="\t")+1
 
-#for K=4, import the 100 run datafile (100 q matrix)
-K4_100runs<-read.table("data/Q_bemigen_K4.indfile",header=FALSE,
+#for K=4, import the 50 run datafile (50 q matrix)
+K4_50runs<-read.table("data/Q_bemigen_K4.indfile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
 #import the column order for the best CLUMPP "permutation"
 K4_colorder<-read.table("data/Q_bemigen_K4.colorder",header=FALSE,
                         blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 54)
-K4_100runs<-split(K4_100runs[,-c(1:5)],rep(1:100,each=54))
+K4_50runs<-split(K4_50runs[,-c(1:5)],rep(1:50,each=54))
 #reordering the columns so that the different repetition colorization fit
-for (i in 1:100){
-  K4_100runs[[i]]<-K4_100runs[[i]][as.numeric(K4_colorder[i,])]
+for (i in 1:50){
+  K4_50runs[[i]]<-K4_100runs[[i]][as.numeric(K4_colorder[i,])]
 }
 #importing the order of the run so that the different repetition 
 #corresponding to the same clustering solution followed each other
--- bemG_str_plot.R
+++ bemG_str_plot.R
@@ -9,18 +9,18 @@ source("bemG_load_data.R")
 
 
 ##############################################################################/
-#Bemisia Q: plot a list of 100 STRUCTURE output files for each K####
+#Bemisia Q: plot a list of 50 STRUCTURE output files for each K####
 ##############################################################################/
 
 coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
 effpop<-c(2,4,1,17,1,9,20)
 poptiquet<-c("8","32","33","34","47","56","320")
-#now we can plot the 100 runs on the same figure
+#now we can plot the 50 runs on the same figure
 #for K=2
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(50,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
+for (i in 1:50){
   j<-as.numeric(K2_reporderQ[i])
-  temp<-K2_100runs[[j]]
+  temp<-K2_50runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=1,
              leg_y="K=2",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
@@ -30,10 +30,10 @@ par(op)
 #export pdf 25 x 12
 
 #for K=3
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(50,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
+for (i in 1:50){
   j<-as.numeric(K3_reporderQ[i])
-  temp<-K3_100runs[[j]]
+  temp<-K3_50runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=1,
              leg_y="K=3",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
@@ -43,10 +43,10 @@ par(op)
 #export pdf 25 x 12
 
 #for K=4
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(50,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
+for (i in 1:50){
   j<-as.numeric(K4_reporderQ[i])
-  temp<-K4_100runs[[j]]
+  temp<-K4_50runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=1,
              leg_y="K=4",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
